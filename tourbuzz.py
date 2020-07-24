a7db24cf33d8c5238497f4f9e464c3805416a22d
milovanderlinden <milo@dogodigi.net>
2020-07-24 12:52:02 +0200

    Remove jobs from main

--- tourbuzz.py
+++ tourbuzz.py
@@ -1,11 +1,11 @@
 import uvicorn
 from api import app
-from jobs import run_jobs
+# from jobs import run_jobs
 
 
 if __name__ == "__main__":
     """
     The main entry point for the API, runs a uvicorn server on port 8000.
     """
-    run_jobs()
+    # run_jobs()
     uvicorn.run(app, host="0.0.0.0", port=8000)
