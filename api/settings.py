7c850fa9c78c87d8faa638acb15dba0673e66260
milovanderlinden <milo@dogodigi.net>
2020-07-24 12:11:36 +0200

    Fixed typo in google maps key

--- api/settings.py
+++ api/settings.py
@@ -6,7 +6,7 @@ from pydantic import AnyHttpUrl, BaseSettings, validator
 
 class Settings(BaseSettings):
     SQL_ALCHEMY_DATABASE_URL = os.environ.get('SQL_ALCHEMY_DATABASE_URL', "postgresql://postgres:mysecretpassword@localhost/gis")
-    GOOGLE_MAPS_KEY = os.environ.get('GOOGLE_MAPS_KEY', "googlemapskeyhere)
+    GOOGLE_MAPS_KEY = os.environ.get('GOOGLE_MAPS_KEY', "googlemapskeyhere")
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [x.strip() for x in os.environ.get(
         'BACKEND_CORS_ORIGINS', "http://localhost,http://localhost:4200,http://localhost:3000").split(',')]
 
